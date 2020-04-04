f698ccba0a8ecb0c2fe8e36ec43f2930153f940e
seebazz <bretsebastian@protonmail.com>
2020-04-03 21:28:50 -0500

    Tuple example

--- app.ts
+++ app.ts
@@ -1,12 +1,18 @@
-// const person: {
-//     name: string;
-//     age: number;
-// } = {
-const person = {
+const person: {
+    name: string;
+    age: number;
+    hobbies: string[];
+    role: [number, string];
+} = {
     name: 'Maximilian',
     age: 30,
-    hobbies: ['Sports', 'Cooking']
+    hobbies: ['Sports', 'Cooking'],
+    role: [2, 'author']
 };
+
+// typescript will not catch a push into a tuple
+// person.role.push('admin')
+
  // any is a do anything you want type
 let favoriteActivities: string[];
 favoriteActivities = ['Sports'];
