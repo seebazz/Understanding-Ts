74334187ccd504f4a8b03ae2642052ea31509de2
seebazz <bretsebastian@protonmail.com>
2020-04-03 21:39:02 -0500

    Added enum example

--- app.ts
+++ app.ts
@@ -1,13 +1,26 @@
-const person: {
-    name: string;
-    age: number;
-    hobbies: string[];
-    role: [number, string];
-} = {
+// const person: {
+//     name: string;
+//     age: number;
+//     hobbies: string[];
+//     role: [number, string];
+// } = {
+//     name: 'Maximilian',
+//     age: 30,
+//     hobbies: ['Sports', 'Cooking'],
+//     role: [2, 'author']
+// };
+
+// const ADMIN = 0;
+// const READ_ONLY = 1;
+// const AUTHOR = 2;
+
+enum Role { ADMIN, READ_ONLY, AUTHOR };
+
+const person = {
     name: 'Maximilian',
     age: 30,
     hobbies: ['Sports', 'Cooking'],
-    role: [2, 'author']
+    role: Role.ADMIN
 };
 
 // typescript will not catch a push into a tuple
@@ -22,4 +35,8 @@ console.log(person.name);
 for (const hobby of person.hobbies) {
     console.log(hobby.toUpperCase());
     // console.log(hobby.map()); // !!! ERROR !!!
+}
+
+if (person.role === Role.ADMIN) {
+    console.log('is admin');
 }
